e9867131b65bca829ce133ec5cdb9e8d47de58d0
Ryan Eldon <47878230+rdogstyle@users.noreply.github.com>
2021-04-05 19:41:02 -0400

    Header and Body Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Newsfeed from './Newsfeed';
+import Stats from './Stats';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			{/* Header */}
+			<div className="app__Header">
+				<Header />
+			</div>
+			{/* Body */}
+			<div className="app__body">
+				<div className="app__container">
+					<Newsfeed />
+					<Stats />
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
