1de4c51b8cb574c1fc224cb25077d6202f18d82a
Ryan Eldon <47878230+rdogstyle@users.noreply.github.com>
2021-04-14 16:24:05 -0400

    Public to Docs Folder Name

--- src/Newsfeed.js
+++ src/Newsfeed.js
@@ -24,8 +24,8 @@ function Newsfeed() {
 			<div className="newsfeed__container">
 				<div className="newsfeed__chartSection">
 					<div className="newsfeed__portfolio">
-						<h1>$420,000</h1>
-						<p>+69.00 (+4.20%) Today</p>
+						<h1>$120,000</h1>
+						<p>+79.00 (+4.30%) Today</p>
 					</div>
 					<div className="newsfeed__chart">
 						<LineGraph />
